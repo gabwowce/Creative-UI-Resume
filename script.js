75ebfc64e745ad6c0ec3bce98c974a342e836d77
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-07-27 16:56:40 +0300

    Add files via upload

--- script.js
+++
@@ -1,3 +0,0 @@
- document.getElementById('html-progress').style.width = '80%';  // HTML žinių lygis - 80%
-    document.getElementById('css-progress').style.width = '70%';   // CSS žinių lygis - 70%
-    document.getElementById('js-progress').style.width = '60%';    // JavaScript žinių lygis - 60%
